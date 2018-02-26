402d5467da1484a17fad5f0a05baeec5d8d1e486
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-02-25 21:59:21 -0500

    search: add search button and result table

--- src/Search.vue
+++ src/Search.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Search TODO</div>
+  <div>
+    <button class="btn btn-default" type="button" v-on:click="searchFiles()">Search</button>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="header in table.headers">{{header}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in table.data">
+          <td>{{row.filename}}</td>
+          <td>{{row.url}}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td v-bind:colspan="table.headers.length">1</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -7,7 +27,36 @@ export default {
   name: 'Search',
   data: function () {
     return {
+      table: {
+        headers: [
+          'filename',
+          'url',
+        ],
+        data: []
+      }
     };
+  },
+  methods: {
+    searchFiles: function () {
+      let vm = this;
+      fetch('https://api.github.com/gists/public')
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (arr) {
+          console.log(arr);
+          // TODO - Reduce this array to a collection of files
+          vm.table.data = arr.map(function (row) {
+            return {
+              filename: row.files[Object.keys(row.files)[0]].filename,
+              url: row.html_url
+            }
+          });
+        })
+        .catch(function (error) {
+          console.error(error);
+        })
+    }
   }
 }
 </script>
