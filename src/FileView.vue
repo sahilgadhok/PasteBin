b33cc0bff46145a9b532713b5fa1599e23785f4d
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-02-27 16:26:50 -0500

    fileview: profile: display ./package.json in File Page

--- src/FileView.vue
+++ src/FileView.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>FileView TODO</div>
+  <div>
+    <div>{{filename}}</div>
+    <pre>{{content}}</pre>
+  </div>
 </template>
 
 <script>
+import fetchPonyfill from 'fetch-ponyfill';
+const {fetch} = fetchPonyfill();
+
 export default {
   name: 'FileView',
   data: function () {
-    return {
+    let data = {
+      filename: 'package.json',
+      content: ''
     };
+
+    fetch(data.filename)
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (text) {
+        data.content = text;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+    return data;
   }
 }
 </script>
--- src/Profile.vue
+++ src/Profile.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Profile TODO</div>
+  <div>
+    <div>Username {{username}}</div>
+    <div>Groups
+      <span v-for="group in groups">{{group}}</span>
+    </div>
+    <div>Files
+      <a v-for="file in files" v-bind:href="file.url">{{file.name}}</a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +15,12 @@ export default {
   name: 'Profile',
   data: function () {
     return {
+      username: 'Foo',
+      groups: ['Bar'],
+      files: [{
+        name: 'package.json',
+        url: './index.html#/file',
+      }]
     };
   }
 }
