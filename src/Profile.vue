aaba7aa43a47490d99e5bc3c8d1620c16a2e8b96
SanchitL <sanchit.luthra@mail.utoronto.ca>
2018-03-03 16:42:39 -0500

    Working on profile page

--- src/Profile.vue
+++ src/Profile.vue
@@ -1,30 +1,73 @@
 <template>
-  <div>
-    <div>Username {{username}}</div>
-    <div>Groups
-      <span v-for="(group, index) in groups" v-bind:key="index">{{group}}</span>
-    </div>
-    <div>Files
-      <a target="_blank" v-for="file in files" v-bind:key="file.url" v-bind:href="file.url">{{file.name}}</a>
-    </div>
-  </div>
+  <div class="container-fluid">
+	<div class="row">
+	<div>
+    <h3 class="profile-header animated slideInLeft">{{profileInfo.name}}'s Profile</h3>
+	</div>
+		<div class="col-md-3">
+			<div>
+				<div class="form-group">
+					<label for="email">Email Address</label>
+					<h4 id="email">{{profileInfo.email}}</h4>
+				</div>
+				<div class="form-group">
+					<label for="userName">User Name</label>
+					<h4 id="userName">{{profileInfo.userName}}</h4>
+				</div>
+        		<div class="form-group">
+          			<label for="friends">Friends</label>
+					<ul>
+						<h4 v-for="friend in profileInfo.friends">
+							{{friend.userName}}
+						</h4>
+					</ul>
+        		</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<h3>Pastes</h3>
+			<ul>
+				<h4 v-for="paste in profileInfo.pastes">
+					{{paste.text}}
+				</h4>
+			</ul>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
+import Vue from 'vue';
+var profileInfo = {
+  name: "Tim Struggles",
+  email: "timstruggles@mail.com",
+  userName: 'TheStruggle',
+  pastes: [],
+	friends: [
+		{userName: "pasterer"},
+		{userName: "teh pasterer"}
+	]
+}
+
 export default {
   name: 'Profile',
   data: function () {
     return {
-      username: 'Foo',
-      groups: ['Bar'],
-      files: [{
-        name: 'gistfile1.txt',
-        url: '#/file?hash=janlazo/a828a18c9ea19b9f84b2cc0c640a870d/raw/04682927b385ecc2eee74c5e40f2e2f022254023/gistfile1.txt'
-      }]
+      profileInfo: profileInfo
     };
   }
 }
 </script>
 
 <style scoped>
+
+.subsection {
+  outline: 1px solid black;
+  padding: 5px;
+}
+
+.profile-header {
+  text-align: center;
+  /* padding-left: 5px; */
+}
 </style>
