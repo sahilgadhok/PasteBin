ca43032e8437b9b40ad8865b4f16cfcfa125c610
Shannon Lee <shannon.k.lee@hotmail.com>
2018-03-04 17:00:19 -0500

    Placeholder comments + changed File Host App to FileHub

--- src/FileView.vue
+++ src/FileView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
+  <div id="container">
     <div>{{filename}}</div>
     <pre>{{content}}</pre>
+    <div class="comments-box">
+      <h3>Comments</h3>
+      <ul>
+        <li v-for="comment in comments">
+          <p class="comment-user">{{ comment.user }}</p>
+          <div class="comment">{{ comment.content }}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -17,7 +26,17 @@ export default {
     const filepathArr = this.hash.split('/');
     const data = {
       filename: filepathArr[filepathArr.length - 1],
-      content: ''
+      content: '',
+      comments: [
+        {
+          user: 'Alice',
+          content: 'Test comment'
+        },
+        {
+          user: 'Bob',
+          content: 'Test comment 2'
+        }
+      ]
     };
 
     axios.get(resourcePrefix + this.hash, {
@@ -36,4 +55,27 @@ export default {
 </script>
 
 <style scoped>
+li {
+  border: 1px solid #000;
+  border-radius: 5px;
+  list-style-type: none;
+  margin: 5px;
+  padding: 10px;
+}
+
+pre {
+  width: 100%;
+}
+
+#container {
+  width: 60%;
+}
+
+.comments-box {
+  width: 80%;
+}
+
+.comment-user {
+  font-size: 14pt
+}
 </style>
