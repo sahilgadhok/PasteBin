ec48c08f0b69f238f2aed599e01f679d192188bd
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-25 00:30:26 -0400

    login: add login/logout buttons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app" class="container-fluid">
     <h1>FileHub</h1>
-    <div class="text-right">
-      <router-link exact to="/profile">Profile</router-link>
-    </div>
+    <form class="text-right">
+      <div v-if="user">
+        <router-link exact to="/profile">Profile</router-link>
+        <button class="btn btn-default" type="button">Logout</button>
+      </div>
+      <div v-else>
+        <label>Username</label>
+        <label>Password</label>
+        <button class="btn btn-default" type="button">Login</button>
+      </div>
+    </form>
     <ul class="nav nav-pills">
       <router-link tag="li" exact active-class="active"
       v-for="route in routes" v-bind:key="route.path" v-bind:to="route.path">
@@ -21,6 +29,7 @@ export default {
   name: 'App',
   data: function () {
     return {
+      user: false,
       // Display public/named routes only
       routes: routes.filter(function (route) {
         return route.name && typeof route.name === 'string';
