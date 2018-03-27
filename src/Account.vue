3ba3d039ef40dae72dae643989c3c5aa73cf9a9d
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-26 21:40:30 -0400

    Add account component for login/logout

---
+++ src/Account.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-if="account">
+    <router-link exact to="/profile">Profile</router-link>
+    <button class="btn btn-default" type="button"
+    v-on:click="logout()">Logout</button>
+  </div>
+  <form class="form-inline" v:on-submit.prevent="onSubmit" v-else>
+    <div class="form-group">
+      <label for="acct-username">Username</label>
+      <input id="acct-username" type="text" v-model="form.username">
+    </div>
+    <div class="form-group">
+      <label for="acct-password">Password</label>
+      <input id="acct-password" type="password" v-model="form.password">
+    </div>
+    <button class="btn btn-default" type="button"
+    v-on:click="login()">Login</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Account',
+  data: function () {
+    return {
+      form: {
+        username: '',
+        password: '',
+      },
+      account: false
+    };
+  },
+  methods: {
+    login: function () {
+      this.account = true;
+    },
+    logout: function () {
+      this.account = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
   <div id="app" class="container-fluid">
     <h1>FileHub</h1>
-    <form class="text-right">
-      <div v-if="user">
-        <router-link exact to="/profile">Profile</router-link>
-        <button class="btn btn-default" type="button">Logout</button>
-      </div>
-      <div v-else>
-        <label>Username</label>
-        <label>Password</label>
-        <button class="btn btn-default" type="button">Login</button>
-      </div>
-    </form>
+    <account class="text-right"></account>
     <ul class="nav nav-pills">
-      <router-link tag="li" exact active-class="active"
+      <router-link tag="li" active-class="active" exact
       v-for="route in routes" v-bind:key="route.path" v-bind:to="route.path">
         <a href="javascript:void(0)">{{route.name}}</a>
       </router-link>
@@ -24,17 +14,20 @@
 
 <script>
 import routes from './routes.js';
+import Account from './Account.vue';
 
 export default {
   name: 'App',
   data: function () {
     return {
-      user: false,
       // Display public/named routes only
       routes: routes.filter(function (route) {
         return route.name && typeof route.name === 'string';
       })
     };
+  },
+  components: {
+    'account': Account
   }
 }
 </script>
