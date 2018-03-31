eac521a9c5cf263706519d7a8fe376dedc1737ae
Shannon Lee <shannon.k.lee@hotmail.com>
2018-03-31 15:56:47 -0400

    global variable for db

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const pgp = require('pg-promise')();
 const app = express();
 var connectionString = 'postgres://filehub_admin:password@filehub.cekghi6bfi1x.us-east-2.rds.amazonaws.com:5432/filehub';
+var db = pgp(connectionString);
 
 app.use('/', express.static(__dirname + '/'));
 
 app.post('/signup', (req, res) => {
-  var db = pgp(connectionString);
   db.none('INSERT INTO users(user_id, username, password) values(DEFAULT, $1, $2)',
     [req.username, req.password])
     .then(function () {
