67f1df01f9df9118a008fc04f3de984f80498558
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-31 17:25:38 -0400

    fileview: anonymous commenting works

--- src/FileView.vue
+++ src/FileView.vue
@@ -9,7 +9,7 @@
         <p>{{comment.content}}</p>
       </li>
     </ul>
-    <form v:on-submit.prevent="onSubmit" v-if="sessionToken">
+    <form v:on-submit.prevent="onSubmit" v-if="inputEnabled">
       <div class="form-group">
         <label for="comment-self">Leave a Comment</label>
         <textarea id="comment-self" class="form-control" rows="5"
@@ -38,6 +38,7 @@ export default {
   },
   data: function () {
     return {
+      inputEnabled: !!firebase,
       comment: '',
       file: {
         name: '',
@@ -48,18 +49,29 @@ export default {
   },
   store: ['sessionToken'],
   methods: {
-    submitComment: function () {
-      if (!this.comment) {
-        return;
-      }
+    updateComments: function (hash) {
+        if (!hash || !firebase) {
+          return;
+        }
 
-      // TODO: submit to server via POST/PUT
-      this.file.comments.push({
-        id: [Date.now(), Date.now()].join('-'),
-        user: 'timStruggle',
-        content: this.comment
-      });
-      this.comment = '';
+        const vm = this;
+        const db = firebase.database();
+        db.ref('/comment/' + hash).orderByChild('created').once('value')
+          .then(function (snapshot) {
+            const comments = snapshot.val();
+            if (!comments) {
+              return;
+            }
+            vm.file.comments = Object.keys(comments).map(key => ({
+              id: key,
+              user: comments[key].user,
+              content: comments[key].content,
+              created: comments[key].created,
+            }))
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
     },
     updateFile: function (hash) {
       if (!hash || !firebase) {
@@ -77,23 +89,26 @@ export default {
 
           vm.file.name = file.name;
           vm.file.content = file.content;
-          return db.ref('/comment/' + hash).once('value');
-        })
-        // comments
-        .then(function (snapshot) {
-          const comments = snapshot.val();
-          if (!comments) {
-            return;
-          }
-          vm.file.comments = Object.keys(comments).map(key => ({
-            id: key,
-            user: comments[key].user,
-            content: comments[key].content
-          }));
+          vm.updateComments(hash);
         })
         .catch(function (error) {
           console.error(error);
         });
+    },
+    submitComment: function () {
+      if (!this.comment || !firebase) {
+        return;
+      }
+
+      const db = firebase.database();
+      const newComment = db.ref('/comment/' + this.hash).push();
+      newComment.set({
+        user: 'timStruggle',
+        content: this.comment,
+        created: (new Date()).toISOString()
+      })
+      this.updateComments(this.hash);
+      this.comment = '';
     },
     makeRowKey: function (row) {
       return [row.id, Date.now()].join('-');
