d8ea05c37a22b7a4c7f47b969343905fe36c09c4
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-31 22:39:23 -0400

    Add user signup and authentication (draft)

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,59 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const argon2 = require('@phc/argon2');
 
-exports.auth = functions.https.onRequest(function (req, res) {
-  if (!req.body.password) {
-    res.status(400).send('Invalid user info');
+// Add new user to the database
+exports.signup = functions.https.onRequest(function (req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('Missing username/password');
     return;
   }
 
-  // TODO: store the hash in the db
-  argon2.hash(req.body.password)
+  const db = admin.database();
+  db.ref('/user/' + req.body.username)
+    .once('value')
+    .then(function (snapshot) {
+      if (snapshot && snapshot.val()) {
+        return Promise.reject(new Error('username is taken'));
+      }
+
+      return argon2.hash(req.body.password);
+    })
     .then(function (hash) {
-      res.status(200).send(hash);
+      db.ref('/user/' + req.body.username).set({
+        password: hash
+      });
+      res.status(200).send('Created ' + req.body.username);
       return true;
     })
     .catch(function (error) {
-      res.status(500).send('Cannot authenticate user');
+      res.status(403).send('Invalid username/password');
+    });
+});
+
+// Check if user exists and validate password
+exports.auth = functions.https.onRequest(function (req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('Missing username/password');
+    return;
+  }
+
+  const db = admin.database();
+  db.ref('/user/' + req.body.username)
+    .once('value')
+    .then(function (snapshot) {
+      if (!snapshot) {
+        return Promise.reject(new Error('Invalid username'));
+      }
+
+      const user = snapshot.val();
+      return argon2.verify(user.password, req.body.password);
+    })
+    .then(function () {
+      // TODO: Send a session token or cookie
+      res.status(200).send('User is validated');
+      return true;
+    })
+    .catch(function (error) {
+      res.status(403).send('Invalid username/password');
     });
 });
