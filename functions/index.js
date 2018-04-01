12d45eadf6ea6959b4e59da6bac6ed5f3e04857b
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-31 21:24:33 -0400

    Add auth function to test argon2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const argon2 = require('@phc/argon2');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.auth = functions.https.onRequest(function (req, res) {
+  if (!req.body.password) {
+    res.status(400).send('Invalid user info');
+    return;
+  }
+
+  // TODO: store the hash in the db
+  argon2.hash(req.body.password)
+    .then(function (hash) {
+      res.status(200).send(hash);
+      return true;
+    })
+    .catch(function (error) {
+      res.status(500).send('Cannot authenticate user');
+    });
+});
